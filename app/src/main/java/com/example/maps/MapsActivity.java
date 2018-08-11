b5a06dd1323dc3b45e89b45229e88a93ea64291e
RomanYershov <roma.willfame@bk.ru>
2018-08-11 22:45:05 +0600

    rename view to map

--- app/src/main/java/com/example/maps/MapsActivity.java
+++ app/src/main/java/com/example/maps/MapsActivity.java
@@ -36,26 +36,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
 
-        mMap.setOnMapClickListener(view -> {
+        mMap.setOnMapClickListener(map -> {
 
-
-            double lat = view.latitude;
-            double lang = view.longitude;
+            double lat = map.latitude;
+            double lang = map.longitude;
 
             LatLng newPosition = new LatLng(lat, lang);
 
